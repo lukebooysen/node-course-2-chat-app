c9c0b21518bdc5873a03c141d38395a94f49e7fa
lukebooysen <lukeedwardbooysen@gmail.com>
2019-04-01 08:48:35 +0200

    Add users class and updateUserList event

--- server/server.js
+++ server/server.js
@@ -50,7 +50,7 @@ io.on('connection', (socket) => {
         var user = users.removeUser(socket.id);
 
         if (user) {
-            io.to(user.room).emit('updatUserList', users.getUserList(user.room));
+            io.to(user.room).emit('updateUserList', users.getUserList(user.room));
             io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
         }
     });
